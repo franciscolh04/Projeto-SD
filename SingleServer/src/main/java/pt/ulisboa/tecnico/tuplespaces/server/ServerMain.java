ed9034622fa5c21a92498736677d313c171b4bc1
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2025-02-28 16:39:27 +0000

    Server correctly starting on specified port

--- SingleServer/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/ServerMain.java
+++ SingleServer/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/ServerMain.java
@@ -1,9 +1,41 @@
 package pt.ulisboa.tecnico.tuplespaces.server;
 
-public class ServerMain {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-      // TODO
+public class ServerMain {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println(ServerMain.class.getSimpleName());
+
+        // Receber e imprimir argumentos
+        System.out.printf("Received %d arguments%n", args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("arg[%d] = %s%n", i, args[i]);
+        }
+
+        // Verificar argumentos
+        if (args.length != 1) {
+            System.err.println("Argument missing!");
+            System.err.println("Usage: mvn exec:java -Dexec.args=<port>");
+            return;
+        }
+
+        // Obter e validar a porta
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port number: " + args[0]);
+            return;
+        }
+
+        // Criar e iniciar o servidor
+        Server server = ServerBuilder.forPort(port).build();
+        server.start();
+        System.out.printf("Server started on port: %d%n", port);
+
+        server.awaitTermination();
     }
 }
-
