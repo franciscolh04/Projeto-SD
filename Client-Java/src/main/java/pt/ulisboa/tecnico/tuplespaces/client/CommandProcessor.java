b3a91fa8b44370d4b19ba53071a6054858c6fe4d
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2025-03-07 01:42:51 +0000

    Fixed Exit bug on Client-Java

--- Client-Java/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/CommandProcessor.java
+++ Client-Java/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/CommandProcessor.java
@@ -58,6 +58,7 @@ public class CommandProcessor {
 
                 case EXIT:
                     exit = true;
+                    clientService.shutdown();
                     break;
 
                 default:
